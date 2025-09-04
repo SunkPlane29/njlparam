e700233980629a3da854a666e386d0300d5158c1
Arthuro <arthur.ps22009@gmail.com>
2025-09-04 17:41:48 -0300

    condensado da lattice

--- main.jl
+++ main.jl
@@ -35,12 +35,15 @@ begin
 
     cond_lb = 0.190 
     cond_ub = 0.260
+    cond_lattice = 0.231 # valor central da lattice
+    sigcond = 0.008 # erro estimado
 
     fixedLamb = 0.64
 end
 
 function loglike(theta::AbstractVector)
     Lambda, G, mc = theta  
+    G = G / Lambda^2 # eu entendo melhor assim :D
 
     M = solvegap(Lambda, G, mc)
     mpi = getmpi(M, Lambda, G, mc)
@@ -48,12 +51,13 @@ function loglike(theta::AbstractVector)
     cond = -cbrt(quarkcond(M, Lambda, G, mc))
     
     #condensate uniform likelihood works to truncate the likelihood
-    if cond < cond_lb || cond > cond_ub
-        return -Inf
-    end
+    #if cond < cond_lb || cond > cond_ub
+    #    return -Inf
+    #end
 
     total_logL = 0.0
 
+    total_logL += logpdf(Normal(cond_lattice, sigcond), cond)
     total_logL += logpdf(Normal(meanmpi, sigmpi), mpi)
     total_logL += logpdf(Normal(meanfpi, sigfpi), fpi)
 
@@ -62,15 +66,15 @@ end
 
 function priortransform(u::AbstractVector)
     theta = zeros(eltype(u), length(u))
-    L_min = 0.45
-    L_max = 0.85
-    G_min = 1.5/L_max^2
-    G_max = 2.5/L_min^2
-    mc_min = 0.003
-    mc_max = 0.007
+    L_min = 0.5
+    L_max = 0.7
+    GL2_min = 1.5
+    GL2_max = 3.5
+    mc_min = 0.004
+    mc_max = 0.006
 
     theta[1] = L_min + (L_max - L_min) * u[1]
-    theta[2] = G_min + (G_max - G_min) * u[2]
+    theta[2] = GL2_min + (GL2_max - GL2_min) * u[2]
     theta[3] = mc_min + (mc_max - mc_min) * u[3]
 
     return theta
@@ -78,12 +82,12 @@ end
 
 function nestedsampling_chain()
     ndims = 3
-    nlive = 20_000
+    nlive = 5_000
     sampler = Nested(ndims, nlive)
     model = NestedModel(loglike, priortransform)
     
     println("Starting nested sampling...")
-    chain = NestedSamplers.sample(model, sampler, dlogz=0.1) # Aumentei um pouco o dlogz 
+    chain = NestedSamplers.sample(model, sampler, dlogz=0.1)
     println("Nested sampling completed.")
 
     serialize("njlparameters_chain.jls", chain)
